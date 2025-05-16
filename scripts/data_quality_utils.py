a924ca8786fdb6104fbd69e0d5f332998e716e29
teshager <teshager8922@gmail.com>
2025-05-16 03:41:23 +0300

    feat: add a detect outliers functionality

--- scripts/data_quality_utils.py
+++ scripts/data_quality_utils.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from scipy.stats import zscore
+
+
+
 def columns_with_significant_missing_values(df, threshold=5):
     """
     Returns a DataFrame listing columns with missing values above a specified threshold (percentage).
@@ -24,3 +28,23 @@ def columns_with_significant_missing_values(df, threshold=5):
     })
 
     return missing_values_df.sort_values(by='#missing_values', ascending=False)
+
+
+
+
+def detect_outliers_zscore(df, column_name, threshold=3):
+    """
+    Detect outliers in a dataframe column based on z-score.
+
+    Parameters:
+        df (pd.DataFrame): Input dataframe.
+        column_name (str): Column to check for outliers.
+        threshold (float): Z-score threshold to identify outliers (default=3).
+
+    Returns:
+        pd.DataFrame: Subset of rows considered outliers.
+    """
+    df = df.copy()
+    df['zscore'] = zscore(df[column_name])
+    outliers = df[(df['zscore'] > threshold) | (df['zscore'] < -threshold)]
+    return outliers
