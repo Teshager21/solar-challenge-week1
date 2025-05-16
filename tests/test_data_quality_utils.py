255aa91dc61a00ac4d05c818434ed4a5837a51bb
teshager <teshager8922@gmail.com>
2025-05-16 03:58:00 +0300

    test: add a test for the detect outliers functionality

--- tests/test_data_quality_utils.py
+++ tests/test_data_quality_utils.py
@@ -1,4 +1,4 @@
-from scripts.data_quality_utils import columns_with_significant_missing_values
+from scripts.data_quality_utils import columns_with_significant_missing_values,detect_outliers_zscore
 import pandas as pd
 
 def test_columns_with_significant_missing_values():
@@ -11,3 +11,19 @@ def test_columns_with_significant_missing_values():
     assert 'B' in result.index
     assert 'A' in result.index
     assert 'C' not in result.index
+
+def test_detect_outliers_zscore_basic():
+    # Create a DataFrame with clear outliers
+    data = {'value': [10]*50 + [12]*50 + [11]*50 + [1000]}
+    df = pd.DataFrame(data)
+
+    outliers = detect_outliers_zscore(df, 'value', threshold=3)
+
+    # Check that 1 outlier is returned
+    assert len(outliers) == 1
+    assert outliers.iloc[0]['value'] == 1000
+
+def test_detect_outliers_zscore_no_outliers():
+    # Data with no outliers
+    data = {'value': [10, 12, 11, 10, 12, 11]}
+    df = pd.DataFrame(data)
